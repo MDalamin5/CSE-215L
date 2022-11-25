d6718358451c54d60b97c72f7d2c4b7b2029b188
MDalamin5 <mdal.amin5@northsouth.edu>
2022-11-26 00:26:52 +0600

    Stack

--- src/Lab_9_stack/Stack.java
+++ src/Lab_9_stack/Stack.java
@@ -1,5 +1,66 @@
 package Lab_9_stack;
 
 public class Stack {
-
+	
+	String str;
+	char[] ch= new char[5];
+	int top=0;
+	
+	Stack()
+	{
+		
+	}
+	Stack(String str)
+	{
+		this.str=str;
+	}
+	
+	
+	
+	public void push(String str)
+	{
+		for(int i=0; i<str.length();i++)
+		{
+			if(str.charAt(i)=='(' )
+			{
+				ch[top]=str.charAt(i);
+				top++;
+			}
+			
+			if(str.charAt(i)==')' )
+			{
+				
+			}
+			
+			
+			
+		}
+	}
+	
+	
+	
+	public char pop()
+	{
+		char c;
+		top--;
+		c=ch[top];
+		ch[top]='\0';
+		
+		return c;
+	}
+	
+	public boolean isEmpty()
+	{
+		if(top<=0)
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	
 }
+
+
+
+
--- src/Lab_9_stack/Test.java
+++ src/Lab_9_stack/Test.java
@@ -1,5 +1,12 @@
 package Lab_9_stack;
 
 public class Test {
-
+	public static void main(String[] args) {
+		
+		Stack ob = new Stack("Hello");
+		ob.push("Hl(");
+		
+		System.out.println(ob.pop());
+		System.out.println(ob.isEmpty());
+	}
 }
